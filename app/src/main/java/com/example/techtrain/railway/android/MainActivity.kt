4aabf8354ffcf559faf0f2c35b26ccc1b0910212
icchy-san <icchy.tech@gmail.com>
2024-02-16 21:21:14 +0900

    added image view

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.techtrain.railway.android
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,5 +13,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.text).text = getString(R.string.main_text)
+        findViewById<ImageView>(R.id.imageView).setColorFilter(Color.BLACK)
     }
 }
