52784fd2de8238ad3ddf69873de0f24b1253bb03
icchy-san <icchy.tech@gmail.com>
2024-02-17 01:46:01 +0900

    support button

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.techtrain.railway.android
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.text).text = getString(R.string.main_text)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Log.d("edit/text", findViewById<EditText>(R.id.plain_text_edit).text.toString())
+            findViewById<TextView>(R.id.text).text =
+                findViewById<EditText>(R.id.plain_text_edit).text.toString()
+        }
     }
 }
